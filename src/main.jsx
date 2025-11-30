8194d7fd0a53f14a57c23250a0d7e82ff5cfcbe7
ErhanTurker01 <erantrk0122@gmail.com>
2025-11-30 10:00:39 +0300

    cas

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,26 @@ import App from "./App.jsx";
 import { WalletProvider } from "./providers/WalletProvider";
 import { NotificationProvider } from "./providers/NotificationProvider";
 
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NotificationProvider>
-      <WalletProvider>
-        <App />
-      </WalletProvider>
+      <QueryClientProvider client={queryClient}>
+        <WalletProvider>
+          <App />
+        </WalletProvider>
+      </QueryClientProvider>
     </NotificationProvider>
   </StrictMode>,
 );
